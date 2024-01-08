22ce427a49b95dcb2318467a0febf65520efeea0
Alejandro Poirier <alejandro.poirier@mercadolibre.cl>
2024-01-08 14:25:12 -0300

    fix error in ci.yml file

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -5,9 +5,10 @@ import (
 	"testing"
 	"time"
 
+	"simple-bank/util"
+
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/require"
-	"github.com/techschool/simplebank/util"
 )
 
 func createRandomUser(t *testing.T) User {
